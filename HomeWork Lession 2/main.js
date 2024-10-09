c36f720fef9b565154861095d4d2bf151de79096
Serhii Sosnytskyi <sosnitskij2021@gmail.com>
2024-10-09 07:49:38 +0300

    Lesson 2 | Task 1 changes

--- HomeWork Lession 2/main.js
+++ HomeWork Lession 2/main.js
@@ -1,19 +1,25 @@
 "use strict";
 
 // Task 1
-function fnCounting(objectCall) {
-  objectCall.count = 0;
+function addParamsToRequest(objectCall) {
+  let count = 0;
   return (newInfo) => {
-    objectCall.count += 1;
+    count += 1;
     return {
       ...objectCall,
-      ...newInfo,
+      data: { ...newInfo },
+      count: count - 1, // if we don't want to start with 0, just remove "-1"
     };
   };
 }
 
-const countingFunction = fnCounting({ "access-token": "qwerty" });
+const sendData = addParamsToRequest({ "access-token": "qwerty" });
 
-console.log(countingFunction({ test: "example1" }));
+const result = sendData({ name: "Serhii", "music taste": `Melomaniac` });
+console.log(result);
 
-console.log(countingFunction({ test: "example2" }));
+const result1 = sendData({ name: "Kevin", "music taste": `Heavy metal` });
+console.log(result1);
+
+const result2 = sendData({ name: "Xristo", "music taste": `Hip-hop`, age: 17 });
+console.log(result2);
