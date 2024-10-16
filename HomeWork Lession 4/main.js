9ccca869d9f4a1062cf015f681916275d4ad9e86
Serhii Sosnytskyi <sosnitskij2021@gmail.com>
2024-10-16 19:36:02 +0300

    Lesson 4 | code refactoring, add fnc to modal

--- HomeWork Lession 4/main.js
+++ HomeWork Lession 4/main.js
@@ -2,15 +2,12 @@
 const loadingText = document.querySelector(`.loading`);
 const characterList = document.querySelector(`.characters-list`);
 const characterShow = document.querySelector(`.character-show`);
-// const btnPrevious = document.querySelector(`.btnPrevious`);
-// const btnNext = document.querySelector(".btnNext");
 const currentPageElement = document.querySelector(`.currentPage`);
 const modal = document.getElementById("modal");
 const modalCharacterInfo = document.getElementById("modalCharacterInfo");
 const closeModal = document.getElementById("closeModal");
 
 let currentPage = 1;
-// btnPrevious.disabled = true;
 let nextPageUrl = null;
 let isFetching = false;
 
@@ -20,7 +17,6 @@ async function displayCharacters(pageUrl) {
     isFetching = true;
 
     const response = await fetch(pageUrl);
-
     if (response.status !== 200) {
       alert("Error fetching data");
       return;
@@ -31,97 +27,80 @@ async function displayCharacters(pageUrl) {
 
     nextPageUrl = parsedData.info.next;
 
-    console.log(parsedData.info.next);
-    console.log(parsedData.info.prev);
-    console.log(parsedData.info.pages);
-
     parsedData.results.forEach((item) => {
-      console.log(item.name, item.status, item.image, item.id);
       characterList.innerHTML += `
         <div class="character-card" data-id="${item.id}" data-name="${item.name}" data-status="${item.status}" data-image="${item.image}">
-      <img src="${item.image}" alt="${item.name} image">
-      <div class="character-info">
-        <h3>${item.name}</h3>
-        <p>Status: ${item.status}</p>
-      </div>
-    </div>`;
+          <img src="${item.image}" alt="${item.name} image">
+          <div class="character-info">
+            <h3>${item.name}</h3>
+            <p>Status: ${item.status}</p>
+          </div>
+        </div>`;
     });
-
-    characterList.addEventListener(`click`, (event) => {
-      const card = event.target.closest(`.character-card`);
-      if (card) {
-        const characterId = card.dataset.id;
-        const characterName = card.dataset.name;
-        const characterStatus = card.dataset.status;
-        const characterImage = card.dataset.image;
-        console.log(`Clicked character ID: ${characterId}`);
-        modalCharacterInfo.innerHTML = `
-        <img src="${characterImage}" alt="${characterName} image">
-      <h3>${characterName}</h3>
-      <p>Status: ${characterStatus}</p>
-    `;
-        modal.style.display = "block";
-      }
-    });
-
-    characterList.addEventListener(`scroll`, () => {
-      if (
-        characterList.scrollTop + characterList.clientHeight >=
-          characterList.scrollHeight - 10 &&
-        nextPageUrl &&
-        !isFetching
-      ) {
-        console.log("Скроллбар на дне морском))))))");
-        currentPage++;
-        displayCharacters(nextPageUrl);
-      }
-    });
-
     currentPageElement.textContent = `Current loaded page: ${currentPage}`;
-    console.log(parsedData);
-
-    btnPrevious.disabled = currentPage === 1;
-    btnNext.disabled = parsedData.info.next === null;
   } catch (error) {
     console.log(`Error:`, error);
   } finally {
     isFetching = false;
     loadingText.style.display = `none`;
-    console.log(`Finally`);
   }
 }
+
 displayCharacters(
   `https://rickandmortyapi.com/api/character/?page=${currentPage}`
 );
 
-// btnNext.addEventListener(`click`, () => {
-//   if (nextPageUrl) {
-//     currentPage++;
-//     displayCharacters(nextPageUrl);
-//   }
-// });
-// btnPrevious.addEventListener(`click`, () => {
-//   if (currentPage > 1) {
-//     currentPage--;
-//     displayCharacters(
-//       `https://rickandmortyapi.com/api/character/?page=${currentPage}`
-//     );
-//   }
-// });
+function openModal() {
+  window.addEventListener("keydown", handleEscapeKey);
+  modal.style.display = "block";
+}
 
-closeModal.addEventListener(`click`, () => {
-  modal.style.display = `none`;
-});
+function closeModalWindow() {
+  modal.style.display = "none";
+  window.removeEventListener("keydown", handleEscapeKey);
+}
 
-window.addEventListener(`click`, (event) => {
-  if (event.target === modal) {
-    modal.style.display = `none`;
+characterList.addEventListener("click", (event) => {
+  const card = event.target.closest(".character-card");
+  if (card) {
+    const characterId = card.dataset.id;
+    const characterName = card.dataset.name;
+    const characterStatus = card.dataset.status;
+    const characterImage = card.dataset.image;
+
+    modalCharacterInfo.innerHTML = `
+      <img src="${characterImage}" alt="${characterName} image">
+      <br>
+      <h3>${characterName}</h3>
+      <p>Status: ${characterStatus}</p>
+      <p>ID: ${characterId}</p>`;
+
+    openModal();
   }
 });
 
-// Просто знаю как это сделать :)
-window.addEventListener(`keydown`, (event) => {
-  if (event.key === `Escape` && modal.style.display === `block`) {
-    modal.style.display = `none`;
+characterList.addEventListener("scroll", () => {
+  if (
+    characterList.scrollTop + characterList.clientHeight >=
+      characterList.scrollHeight - 10 &&
+    nextPageUrl &&
+    !isFetching
+  ) {
+    currentPage++;
+    displayCharacters(nextPageUrl);
+  }
+});
+
+closeModal.addEventListener("click", closeModalWindow);
+
+function handleEscapeKey(addKey) {
+  if (addKey.key === "Escape") {
+    closeModalWindow();
+  }
+}
+
+window.addEventListener("click", (event) => {
+  if (event.target === modal) {
+    closeModalWindow();
   }
 });
