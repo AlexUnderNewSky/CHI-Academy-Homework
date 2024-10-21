52f5de4589935a185f186a30cda52d6be28f07a0
Serhii Sosnytskyi <sosnitskij2021@gmail.com>
2024-10-21 15:43:48 +0300

    HomeWork 5 | add useCallback, UI error handling, image loading optimisation

--- HomeWork Lesson 5/src/App.js
+++ HomeWork Lesson 5/src/App.js
@@ -1,10 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 
 const App = () => {
   const [loading, setLoading] = useState(true);
   const [characters, setCharacters] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [totalPages, setTotalPages] = useState(0);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     const displayCharacters = async () => {
@@ -14,16 +15,15 @@ const App = () => {
           `https://rickandmortyapi.com/api/character/?page=${currentPage}`
         );
 
-        if (response.status !== 200) {
-          alert("Error fetching data");
-          return;
+        if (!response.ok) {
+          throw new Error("Error fetching data");
         }
 
         const parsedData = await response.json();
         setCharacters(parsedData.results);
         setTotalPages(parsedData.info.pages);
       } catch (error) {
-        console.log("Error:", error);
+        setError(error.message);
       } finally {
         setLoading(false);
       }
@@ -32,28 +32,34 @@ const App = () => {
     displayCharacters();
   }, [currentPage]);
 
-  const handleNextPage = () => {
+  const handleNextPage = useCallback(() => {
     if (currentPage < totalPages) {
-      setCurrentPage(currentPage + 1);
+      setCurrentPage((prevPage) => prevPage + 1);
     }
-  };
+  }, [currentPage, totalPages]);
 
-  const handlePreviousPage = () => {
+  const handlePreviousPage = useCallback(() => {
     if (currentPage > 1) {
-      setCurrentPage(currentPage - 1);
+      setCurrentPage((prevPage) => prevPage - 1);
     }
-  };
+  }, [currentPage]);
 
   return (
     <div>
-      <div className="loading" style={{ display: loading ? "block" : "none" }}>
-        Loading...
-      </div>
+      {loading && <div className="loading">Loading...</div>}
+      {error && <div className="error">{error}</div>} {}
       <h1 class="title">Character List</h1>
       <div className="characters-list">
         {characters.map((item) => (
           <div className="character-card" key={item.id}>
-            <img src={item.image} alt={`${item.name} image`} />
+            <img
+              src={item.image}
+              alt={`${item.name} image`}
+              onError={(e) => {
+                e.target.src =
+                  "https://developers.google.com/static/maps/documentation/streetview/images/error-image-generic.png";
+              }}
+            />
             <div className="character-info">
               <h3>{item.name}</h3>
               <p>Status: {item.status}</p>
