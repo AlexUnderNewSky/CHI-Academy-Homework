8d8a7b397a2bdb00017e3997a3ccdd757ddb4ff1
Serhii Sosnytskyi <sosnitskij2021@gmail.com>
2024-11-04 22:30:20 +0200

    Homework | Lesson 8 update pagination data.data

--- HomeWork Lesson 8/src/api/exhibitActions.tsx
+++ HomeWork Lesson 8/src/api/exhibitActions.tsx
@@ -3,9 +3,9 @@ import { axiosInstance } from "./axiosInstance"; // Import your axios instance
 export const fetchExhibits = async () => {
   try {
     const response = await axiosInstance.get("api/exhibits");
-    return response.data; 
+    return response.data.data;
   } catch (error) {
-    throw error; 
+    throw error;
   }
 };
 
